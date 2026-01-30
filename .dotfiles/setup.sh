c3c67bfe2168bf7926879695e389f4bb9c16c804
Michael Jang <michaeljang86@gmail.com>
2026-01-30 17:09:26 -0500

    Source Homebrew and nvm settings on setup

--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -1,8 +1,9 @@
-source ~/.zshrc
-
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Set up nvm and node
+source ~/.zshrc    # source brew settings
 brew install nvm
+
+source ~/.zshrc    # source nvm settings
 nvm install --lts
